be7994cc6a96f6e36e7e3641da2bd544f655bfcc
Roberto Beltran <dev.rober@outlook.com>
2024-02-22 14:19:44 -0700

    se crea entidad Book

---
+++ src/main/java/com/roberto/hexagonal/entity/Book.java
@@ -0,0 +1,26 @@
+package com.roberto.hexagonal.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String title;
+	
+	private String description;
+	
+	private Double price;
+}
