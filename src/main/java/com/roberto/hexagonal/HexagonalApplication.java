39a4a9b52b81f6bb7b04fae38708af52f44c736c
Roberto Beltran <dev.rober@outlook.com>
2024-02-22 14:19:31 -0700

    se corrige error de sintaxis en clase main HexagonalApplication

--- src/main/java/com/roberto/hexagonal/HexagonalApplication.java
+++ src/main/java/com/roberto/hexagonal/HexagonalApplication.java
@@ -2,9 +2,8 @@ package com.roberto.hexagonal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
-import com.roberto.hexagonal.data.BookDTO;
+import com.roberto.hexagonal.data.BookDto;
 
 @SpringBootApplication
 public class HexagonalApplication {
@@ -12,8 +11,7 @@ public class HexagonalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HexagonalApplication.class, args);
 		
-		BookDTO book = new BookDTO(12L, "Hola", "Hola", 23.53);
+		BookDto book = new BookDto(12L, "Hola", "Hola", 23.53);
 		System.out.println(book);
 	}
-L
 }
