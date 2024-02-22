8f983f2021e9f526611c9da512202691e424fe0f
Roberto Beltran <dev.rober@outlook.com>
2024-02-22 14:44:32 -0700

    agregamos el controlador Rest BookController en la ruta /book

---
+++ src/main/java/com/roberto/hexagonal/application/controller/BookController.java
@@ -0,0 +1,49 @@
+package com.roberto.hexagonal.application.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.roberto.hexagonal.data.BookDto;
+import com.roberto.hexagonal.ports.api.BookServicePort;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+	
+	@Autowired
+	private BookServicePort bookServicePort;
+
+	
+	@PostMapping("/add") 
+	public BookDto addBook(@RequestBody BookDto bookDto) { 
+		return bookServicePort.addBook(bookDto);
+	}
+	
+	@PutMapping("/update") 
+	public BookDto updateBook(@RequestBody BookDto bookDto) { 
+		return bookServicePort.updateBook(bookDto);
+	}
+	
+	@GetMapping("/get/{id}") 
+	public BookDto getBookByID(@PathVariable long id) { 
+		return bookServicePort.getBookById(id);
+	}
+	
+	@GetMapping("/get") 
+	public List<BookDto> getAllBooks() { 
+		return bookServicePort.getBooks();
+	}
+	
+	@DeleteMapping("/delete/{id}") 
+	public void deleteBookByID(@PathVariable long id) { bookServicePort.deleteBookById(id);
+	}
+}
