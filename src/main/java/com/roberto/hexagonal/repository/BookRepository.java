7caba03a6d7280e38f131de5ea85b15a8269dda0
Roberto Beltran <dev.rober@outlook.com>
2024-02-22 14:22:36 -0700

    se crea repositorio Book, una interfaz que extiende JpaRepository

---
+++ src/main/java/com/roberto/hexagonal/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.roberto.hexagonal.repository;
+
+import com.roberto.hexagonal.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long>{
+
+}
